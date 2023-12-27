04d81bef07049d78aa5c9edaaa8049afeb387dde
Denis Platonov <146228938+deplat@users.noreply.github.com>
2023-12-27 13:06:08 +0300

    Exclude node_modules and minor changes before PR

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const v1KotoRouter = require('./v1/routes/kotoRoutes');
 
 const app = express();
 const cache = apicache.middleware;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const connectKotoDB = () => {
   mongoose.connect(uri, { dbName: "kotolog" })
