af66a89a7c069a8c0dbffda9e530d181e9617af7
dan <146228938+deplat@users.noreply.github.com>
2024-07-24 09:41:10 +0300

    Added unclaimed prop for cat

--- app/cats/page.tsx
+++ app/cats/page.tsx
@@ -18,8 +18,6 @@ export default async function Cats() {
             }
         }
     });
-    console.log(cats);
-
     return (
         <div className="bg-gray-50">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
