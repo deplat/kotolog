df9d22b072295778439b4fa6741ca7ecd16c3d2d
dan <146228938+deplat@users.noreply.github.com>
2024-07-27 10:25:01 +0300

    Fix album upload

--- app/cats/[id]/layout.tsx
+++ app/cats/[id]/layout.tsx
@@ -1,16 +1,7 @@
-import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({subsets: ["cyrillic"]});
 
-export const metadata: Metadata = {
-    title: {
-        template: '%s | kotolog',
-        default: 'kotolog',
-    },
-    description: 'next.js kotolog app',
-};
 
 export default async function RootLayout({
                                              children,
@@ -20,8 +11,12 @@ export default async function RootLayout({
     return (
         <html lang="ru">
         <body className={inter.className} style={{backgroundColor: "#F5F7FA"}}>
-
-        <main className="min-h-screen bg-main">
+        <header
+            className="w-screen h-72 border-t-4 border-b-2 bg-white"
+            style={{borderColor: "#CBD2D9", borderTopColor: "#F35627"}}
+        >
+        </header>
+        <main className="min-h-screen">
             {children}
         </main>
         <footer
