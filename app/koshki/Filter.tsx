b571dbceac7833bc5b061c6de09f9ee62516ba19
Denis Platonov <146228938+deplat@users.noreply.github.com>
2024-08-06 20:32:00 +0300

    Filter

--- app/koshki/Filter.tsx
+++ app/koshki/Filter.tsx
@@ -85,7 +85,8 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
             {/* Filter Button */}
             <button
                 onClick={() => setIsFilterVisible(!isFilterVisible)}
-                className={`fixed bottom-4 right-4 flex flex-row items-center justify-center gap-x-2 p-3 border-2 border-orange-500 rounded-full bg-white shadow-lg z-50`}
+                aria-label={isFilterVisible ? "Скрыть фильтр" : "Показать фильтр"}
+                className={`fixed bottom-6 right-6 flex flex-row items-center justify-center p-4 border-2 border-orange-500 rounded-full bg-white shadow-lg z-50`}
             >
                 {isFilterVisible ? (
                     <FaXmark className='text-orange-500'/>
@@ -101,21 +102,25 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                 {/* Filter Bar */}
                 <div
                     ref={filterRef}
-                    className="flex flex-wrap gap-y-2 w-full max-w-2xl p-4 rounded-md bg-white shadow-lg"
+                    className="flex flex-wrap gap-y-2 w-full max-w-2xl p-4 rounded-md bg-white shadow-lg border-b border-b-orange-500"
+                    role="region"
+                    aria-label="Панель фильтрации"
                 >
                     <div className="flex flex-wrap gap-x-2 gap-y-2 flex-grow">
                         <div className="relative">
                             <button
                                 className={`rounded-md border px-4 py-2 text-sm text-gray-700 font-medium ${gender && 'border-orange-500'} bg-white hover:bg-gray-50`}
                                 onClick={() => toggleDropdown('gender')}
+                                aria-haspopup="listbox"
+                                aria-expanded={showGender}
+                                aria-label="Пол"
                             >
                                 Пол
                             </button>
                             {showGender && (
                                 <div
                                     className="absolute mt-2 w-36 rounded-md backdrop-blur-lg bg-white/75 shadow-md z-50">
-                                    <div className='p-2' role="menu" aria-orientation="vertical"
-                                         aria-labelledby="options-menu">
+                                    <div className='p-2' role="listbox" aria-labelledby="Пол">
                                         <button
                                             className={`w-full px-4 py-2 text-sm font-medium rounded-md hover:bg-orange-500 hover:text-white ${gender === '' ? 'bg-orange-500 text-white' : 'text-gray-700'}`}
                                             onClick={() => handleGenderChange('')}>Любой
@@ -137,14 +142,16 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                             <button
                                 className={`rounded-md border px-4 py-2 text-sm font-medium text-gray-700 ${furType && 'border-orange-500'} bg-white hover:bg-gray-50`}
                                 onClick={() => toggleDropdown('furType')}
+                                aria-haspopup="listbox"
+                                aria-expanded={showFurType}
+                                aria-label="Тип шерсти"
                             >
                                 Тип&nbsp;шерсти
                             </button>
                             {showFurType && (
                                 <div
                                     className="absolute mt-2 w-36 rounded-md backdrop-blur-lg bg-white/75 shadow-md z-50">
-                                    <div className='p-2' role="menu" aria-orientation="vertical"
-                                         aria-labelledby="options-menu">
+                                    <div className='p-2' role="listbox" aria-labelledby="Тип шерсти">
                                         <button
                                             className={`w-full px-4 py-2 text-sm font-medium rounded-md hover:bg-orange-500 hover:text-white ${furType === '' ? 'bg-orange-500 text-white' : 'text-gray-700'}`}
                                             onClick={() => handleFurTypeChange('')}>Любой
@@ -166,14 +173,16 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                             <button
                                 className={`rounded-md border px-4 py-2 text-sm text-gray-700 font-medium ${colors.length > 0 && 'border-orange-500'} bg-white hover:bg-gray-50`}
                                 onClick={() => toggleDropdown('colors')}
+                                aria-haspopup="listbox"
+                                aria-expanded={showColors}
+                                aria-label="Окрас"
                             >
                                 Окрас
                             </button>
                             {showColors && (
                                 <div
                                     className="absolute mt-2 w-56 rounded-md backdrop-blur-lg bg-white/75 shadow-md z-50">
-                                    <div className="p-2" role="menu" aria-orientation="vertical"
-                                         aria-labelledby="options-menu">
+                                    <div className="p-2" role="listbox" aria-labelledby="Окрас">
                                         {uniqueColors.map((color) => (
                                             <div key={color} className="block px-4 py-2 text-sm text-gray-700">
                                                 <label className="inline-flex items-center">
@@ -182,6 +191,8 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                                                         className="form-checkbox"
                                                         checked={colors.includes(color)}
                                                         onChange={() => handleColorChange(color)}
+                                                        aria-checked={colors.includes(color)}
+                                                        aria-label={color}
                                                     />&nbsp;&nbsp;{color}
                                                 </label>
                                             </div>
@@ -195,14 +206,16 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                             <button
                                 className={`rounded-md border px-4 py-2 text-sm text-gray-700 font-medium ${age && 'border-orange-500'} bg-white hover:bg-gray-50`}
                                 onClick={() => toggleDropdown('age')}
+                                aria-haspopup="listbox"
+                                aria-expanded={showAge}
+                                aria-label="Возраст"
                             >
                                 Возраст
                             </button>
                             {showAge && (
                                 <div
                                     className="absolute mt-2 w-36 rounded-md backdrop-blur-lg bg-white/75 shadow-md z-50">
-                                    <div className='p-2' role="menu" aria-orientation="vertical"
-                                         aria-labelledby="options-menu">
+                                    <div className='p-2' role="listbox" aria-labelledby="Возраст">
                                         <button
                                             className={`w-full px-4 py-2 text-sm font-medium rounded-md hover:bg-orange-500 hover:text-white ${age === '' ? 'bg-orange-500 text-white' : 'text-gray-700'}`}
                                             onClick={() => handleAgeChange('')}>Любой
@@ -224,16 +237,18 @@ export const Filter = ({uniqueColors, onFilterChange}: FilterProps) => {
                     {/* Buttons container */}
                     <div className="relative ms-auto flex gap-x-2">
                         <button
-                            onClick={applyFilters}
-                            className="ms-auto px-4 py-2 bg-orange-500 text-sm text-white rounded-md"
-                        >
-                            Показать
-                        </button>
-                        <button
                             onClick={resetFilters}
-                            className="px-4 py-2 bg-red-500 text-sm text-white rounded-md"
+                            className="px-4 py-2 text-sm text-red-600 underline-offset-4 rounded-md hover:underline"
+                            aria-label="Сбросить фильтры"
                         >
                             Сбросить
+                        </button>
+                        <button
+                            onClick={applyFilters}
+                            className="px-4 py-2 bg-orange-500 text-sm text-white rounded-md"
+                            aria-label="Применить фильтры"
+                        >
+                            Показать
                         </button>
                     </div>
                 </div>
