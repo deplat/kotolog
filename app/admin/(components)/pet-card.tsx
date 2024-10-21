ff75b5a227940bf20bf73be50d10626a0e215307
deplat <146228938+deplat@users.noreply.github.com>
2024-10-21 14:28:46 +0300

    pet-card.tsx

---
+++ app/admin/(components)/pet-card.tsx
@@ -0,0 +1,64 @@
+'use client'
+import Image from 'next/image'
+import { IoPencil, IoTrashBin } from 'react-icons/io5'
+import { Button, Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import clsx from 'clsx'
+
+export const PetCard = ({
+  id,
+  name,
+  slug,
+  avatarSrc,
+}: {
+  id: number
+  name: string
+  slug: string
+  avatarSrc?: string
+}) => {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+  const router = useRouter()
+  return (
+    <div className="flex w-full border border-stone-950 p-3">
+      <Image src={avatarSrc || ''} alt={name} width={100} height={100} />
+      <span>{name}</span>
+      <span className="me-auto">{slug}</span>
+      <Button onClick={() => router.push(`/admin/editor/${id}`)} className="hover:text-blue-600">
+        <IoPencil size={24} />
+      </Button>
+      <>
+        <Button onClick={() => setIsDeleteDialogOpen(true)} className="data-[hover]:text-red-600">
+          <IoTrashBin size={24} />
+        </Button>
+        <Dialog
+          open={isDeleteDialogOpen}
+          onClose={() => setIsDeleteDialogOpen(false)}
+          className="relative z-50"
+        >
+          <div className="fixed inset-0 flex w-screen items-center justify-center bg-stone-300/75">
+            <DialogPanel className="flex max-w-lg flex-col items-center gap-y-3 border border-stone-950 bg-stone-100 p-4">
+              <DialogTitle className="flex w-full text-xl font-semibold">Delete pet?</DialogTitle>
+              <Description>This action will permanently delete pet:</Description>
+              <Image src={avatarSrc || ''} alt={name} width={125} height={125} />
+              <span className="text-lg font-semibold">{name}</span>
+              <div className="flex gap-x-3">
+                <Button className="px-4 py-2.5 text-red-600 underline-offset-4">Delete</Button>
+
+                <Button
+                  onClick={() => setIsDeleteDialogOpen(false)}
+                  className={clsx(
+                    'px-4 py-2.5 underline-offset-4 ring-1 ring-inset ring-stone-950',
+                    'data-[hover]:bg-stone-950 data-[hover]:text-stone-100 data-[hover]:underline'
+                  )}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </DialogPanel>
+          </div>
+        </Dialog>
+      </>
+    </div>
+  )
+}
--- app/admin/(modules)/pet-list.tsx
+++ app/admin/(modules)/pet-list.tsx
@@ -1,21 +1,16 @@
 import { getPets } from '@/app/admin/editor/(data-access)/pet'
-import { IoPencil, IoTrashBin } from 'react-icons/io5'
-import Image from 'next/image'
+import { PetCard } from '@/app/admin/(components)/pet-card'
 
 export const PetList = async () => {
   const pets = await getPets()
   return (
     <div className="flex h-fit flex-col border border-stone-950 p-3">
-      <span className="mb-2.5 text-2xl">Pets</span>
+      <h3 className="mb-2.5 text-2xl">Pets</h3>
       {pets ? (
         <ul className="flex flex-col gap-y-3">
           {pets.map((pet, index) => (
-            <li key={index} className="flex min-w-80 items-center border border-stone-950 p-3">
-              <Image src={pet.avatar?.src || ''} alt={pet.name} width={100} height={100} />
-              <span>{pet.name}</span>
-              <span className="me-auto">{pet.slug}</span>
-              <IoPencil size={20} />
-              <IoTrashBin size={20} className="hover:text-red-600" />
+            <li key={index} className="flex">
+              <PetCard id={pet.id} name={pet.name} slug={pet.slug} avatarSrc={pet.avatar?.src} />
             </li>
           ))}
         </ul>
