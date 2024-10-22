4df5e66c98450deb1efe7137838e6b2ff772cb09
deplat <146228938+deplat@users.noreply.github.com>
2024-10-22 13:18:23 +0300

    refactor

--- app/admin/_modules/nav/index.tsx
+++ app/admin/_modules/nav/index.tsx
@@ -1,28 +1,24 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
-import { Button } from '@headlessui/react'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 export const Nav = () => {
-  const router = useRouter()
   return (
     <div className="flex w-full items-center gap-x-2 border-t border-stone-950 bg-stone-100 px-2 py-2">
       {[
-        { label: 'Dashboard', onClick: () => router.push('/admin') },
-        { label: 'New Pet', onClick: () => router.push('/admin/newPet') },
-        { label: 'New Color', onClick: () => router.push('/admin/editor/newColor') },
-      ].map((item, index) => (
-        <Button
-          onClick={item.onClick}
+        { label: 'Dashboard', href: '/admin' },
+        { label: 'New Pet', href: '/admin/newPet' },
+        { label: 'New Color', href: '/admin/editor/newColor' },
+      ].map((link, index) => (
+        <Link
           key={index}
+          href={link.href}
           className={clsx(
             'flex items-center border border-stone-950 bg-stone-100 px-4 py-2.5 text-stone-950 underline-offset-4 transition-all duration-100 first:me-auto',
-            'data-[hover]:bg-stone-950 data-[hover]:text-stone-100 data-[hover]:underline'
+            'hover:bg-stone-950 hover:text-stone-100 hover:underline'
           )}
         >
-          <span>{item.label}</span>
-        </Button>
+          <span>{link.label}</span>
+        </Link>
       ))}
     </div>
   )
