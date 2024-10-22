f1da0146b6de1cfa94d26ce37d7484a9677fa296
deplat <146228938+deplat@users.noreply.github.com>
2024-10-22 15:26:17 +0300

    Highlight current route in /admin navbar

--- app/admin/_modules/nav/index.tsx
+++ app/admin/_modules/nav/index.tsx
@@ -1,11 +1,14 @@
+'use client'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export const Nav = () => {
+  const pathname = usePathname()
   return (
     <div className="flex w-full items-center gap-x-2 border-t border-stone-950 bg-stone-100 px-2 py-2">
       {[
-        { label: 'Dashboard', href: '/admin' },
+        { label: 'Admin', href: '/admin' },
         { label: 'New Pet', href: '/admin/newPet' },
         { label: 'New Color', href: '/admin/editor/newColor' },
       ].map((link, index) => (
@@ -13,8 +16,9 @@ export const Nav = () => {
           key={index}
           href={link.href}
           className={clsx(
-            'flex items-center border border-stone-950 bg-stone-100 px-4 py-2.5 text-stone-950 underline-offset-4 transition-all duration-100 first:me-auto',
-            'hover:bg-stone-950 hover:text-stone-100 hover:underline'
+            'flex items-center border border-stone-950 px-4 py-2.5 underline-offset-4 transition-all duration-100 first:me-auto',
+            'hover:bg-stone-950 hover:text-stone-100 hover:underline',
+            pathname == link.href ? 'bg-stone-950 text-stone-100' : 'bg-stone-100 text-stone-950'
           )}
         >
           <span>{link.label}</span>
