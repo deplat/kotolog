860dffd67349caf40f68c72318a9ec4bb12b57fa
deplat <146228938+deplat@users.noreply.github.com>
2024-11-01 21:17:46 +0300

    Update page.tsx

--- app/admin/pets/page.tsx
+++ app/admin/pets/page.tsx
@@ -7,7 +7,7 @@ export default async function Pets({
   searchParams: Promise<{ withAvatar: boolean }>
 }) {
   const query = await searchParams
-  const withAvatar = query.withAvatar
+  const withAvatar = await query.withAvatar
   return (
     <div className="flex h-full w-full items-center justify-center">
       <AvatarToggleLink />
