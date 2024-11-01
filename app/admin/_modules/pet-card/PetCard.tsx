d58c91b59ffef0388cce564b321041c42154d38e
deplat <146228938+deplat@users.noreply.github.com>
2024-11-01 13:50:28 +0300

    PetCard as reusable component

--- app/admin/_modules/pet-card/PetCard.tsx
+++ app/admin/_modules/pet-card/PetCard.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import Image, { StaticImageData } from 'next/image'
 import { IDropdownMenu } from '@/app/admin/_components/menus/IDropdownMenu'
@@ -5,6 +6,8 @@ import { icons } from '@/lib/styling/icons'
 import { useState } from 'react'
 import { Button, Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import clsx from 'clsx'
+import { deletePet } from '@/app/admin/_data-access'
+import { useRouter } from 'next/navigation'
 
 interface PetCardProps {
   id: number
@@ -12,22 +15,15 @@ interface PetCardProps {
   slug: string
   avatarSrc?: string | StaticImageData
   showAvatar?: boolean
-  onEditPet: () => void
-  onDeletePet: () => void
 }
-export const PetCard = ({
-  id,
-  name,
-  slug,
-  avatarSrc,
-  showAvatar,
-  onEditPet,
-  onDeletePet,
-}: PetCardProps) => {
+export const PetCard = ({ id, name, slug, avatarSrc, showAvatar }: PetCardProps) => {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+  const router = useRouter()
+  const onEditPet = () => router.push(`/admin/${id}`)
+  const onDeletePet = async () => await deletePet(id)
   const showDeleteDialog = () => setIsDeleteDialogOpen(true)
   return (
-    <div className="flex min-w-80 ring-1 ring-stone-950">
+    <div className="flex w-full min-w-80 bg-white ring-1 ring-stone-950">
       <Link href={`/koshki/${slug}`} className="flex w-full">
         {showAvatar ? (
           <div className="relative aspect-1 h-32 w-32">
@@ -39,48 +35,43 @@ export const PetCard = ({
           <div className="text-lg">{name}</div>
           <div className="text-stone-600">/{slug}</div>
         </div>
-        <div>
-          <IDropdownMenu
-            menuButton={{ leftIcon: icons.dotsVertical }}
-            menuItems={[
-              { id: '1', label: 'Edit', onClick: onEditPet },
-              { id: '2', label: 'Delete', onClick: showDeleteDialog, variant: 'warning' },
-            ]}
-          />
-          <Dialog
-            open={isDeleteDialogOpen}
-            onClose={() => setIsDeleteDialogOpen(false)}
-            className="relative z-50"
-          >
-            <div className="fixed inset-0 flex w-screen items-center justify-center bg-stone-300/75">
-              <DialogPanel className="flex max-w-lg flex-col items-center gap-y-3 border border-stone-950 bg-stone-100 p-4">
-                <DialogTitle className="flex w-full text-xl font-semibold">Delete pet?</DialogTitle>
-                <Description>This action will permanently delete pet:</Description>
-                <Image src={avatarSrc || ''} alt={name} width={125} height={125} />
-                <span className="text-lg font-semibold">{name}</span>
-                <div className="flex gap-x-3">
-                  <Button
-                    onClick={onDeletePet}
-                    className="px-4 py-2.5 text-red-600 underline-offset-4"
-                  >
-                    Delete
-                  </Button>
-
-                  <Button
-                    onClick={() => setIsDeleteDialogOpen(false)}
-                    className={clsx(
-                      'px-4 py-2.5 underline-offset-4 ring-1 ring-inset ring-stone-950',
-                      'data-[hover]:bg-stone-950 data-[hover]:text-stone-100 data-[hover]:underline'
-                    )}
-                  >
-                    Cancel
-                  </Button>
-                </div>
-              </DialogPanel>
-            </div>
-          </Dialog>
-        </div>
       </Link>
+      <IDropdownMenu
+        menuButton={{ leftIcon: icons.dotsVertical }}
+        menuItems={[
+          { id: '1', label: 'Edit', onClick: onEditPet },
+          { id: '2', label: 'Delete', onClick: showDeleteDialog, variant: 'warning' },
+        ]}
+      />
+      <Dialog
+        open={isDeleteDialogOpen}
+        onClose={() => setIsDeleteDialogOpen(false)}
+        className="relative z-90"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center bg-stone-300/75">
+          <DialogPanel className="flex max-w-lg flex-col items-center gap-y-3 border border-stone-950 bg-stone-100 p-4">
+            <DialogTitle className="flex w-full text-xl font-semibold">Delete pet?</DialogTitle>
+            <Description>This action will permanently delete pet:</Description>
+            <Image src={avatarSrc || ''} alt={name} width={125} height={125} />
+            <span className="text-lg font-semibold">{name}</span>
+            <div className="flex gap-x-3">
+              <Button onClick={onDeletePet} className="px-4 py-2.5 text-red-600 underline-offset-4">
+                Delete
+              </Button>
+
+              <Button
+                onClick={() => setIsDeleteDialogOpen(false)}
+                className={clsx(
+                  'px-4 py-2.5 underline-offset-4 ring-1 ring-inset ring-stone-950',
+                  'data-[hover]:bg-stone-950 data-[hover]:text-stone-100 data-[hover]:underline'
+                )}
+              >
+                Cancel
+              </Button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
     </div>
   )
 }
---
+++ app/admin/_modules/pet-list/PetList.tsx
@@ -0,0 +1,28 @@
+import { getCachedPets } from '@/app/admin/_data-access/pet'
+import { PetCard } from '@/app/admin/_modules/pet-card'
+
+export const PetList = async () => {
+  const pets = await getCachedPets()
+  return (
+    <div className="flex h-fit w-full max-w-xl flex-col border border-stone-950 bg-stone-50 p-3 shadow-md">
+      <h3 className="mb-2.5 text-2xl">Pets</h3>
+      {pets ? (
+        <ul className="flex flex-col gap-y-3">
+          {pets.map((pet, index) => (
+            <li key={index} className="flex">
+              <PetCard
+                id={pet.id}
+                name={pet.name}
+                slug={pet.slug}
+                avatarSrc={pet.avatar?.src}
+                showAvatar
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Can't get pets, please try again later :(</div>
+      )}
+    </div>
+  )
+}
