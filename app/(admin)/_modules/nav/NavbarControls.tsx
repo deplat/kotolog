19df9c759657921ebe9990c7abf99e94d32965e5
deplat <146228938+deplat@users.noreply.github.com>
2024-11-08 15:39:01 +0300

    New Navbar for /admin routes

--- app/(admin)/_modules/nav/NavbarControls.tsx
+++ app/(admin)/_modules/nav/NavbarControls.tsx
@@ -3,8 +3,8 @@ import { PawPrint, Plus, Search, Menu, SwatchBook } from 'lucide-react'
 
 export const NavbarControls = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex justify-center sm:bottom-8">
-      <nav className="flex w-full justify-between p-3 sm:max-w-xs">
+    <div className="fixed bottom-0 left-0 right-0 flex justify-center sm:bottom-6">
+      <nav className="flex w-full justify-between bg-stone-100/35 px-4 py-2.5 backdrop-blur sm:max-w-xs sm:rounded-full sm:shadow sm:ring-1 sm:ring-stone-700">
         {[
           {
             title: 'Pets',
@@ -36,7 +36,7 @@ export const NavbarControls = () => {
             <item.icon
               size={30}
               absoluteStrokeWidth
-              className="relative transition group-hover:scale-125"
+              className="relative transition group-hover:scale-125 group-hover:text-orange-600"
             />
           </Link>
         ))}
