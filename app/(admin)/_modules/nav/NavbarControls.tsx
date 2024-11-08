a4bf93c8c96515f606843b0adc431f66e5ea112a
deplat <146228938+deplat@users.noreply.github.com>
2024-11-09 00:06:49 +0300

    DropdownMenu in NavbarControls

--- app/(admin)/_modules/nav/NavbarControls.tsx
+++ app/(admin)/_modules/nav/NavbarControls.tsx
@@ -1,54 +1,102 @@
 'use client'
 
 import Link from 'next/link'
-import { PawPrint, Plus, Search, Menu, SwatchBook } from 'lucide-react'
+import {
+  PawPrint,
+  Plus,
+  Search,
+  Menu as MenuIcon,
+  SwatchBook,
+  Sun,
+  Moon,
+  SunMoon,
+  Home,
+  LogOut,
+} from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
+import { Button, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
 export const NavbarControls = () => {
   const pathname = usePathname()
   console.log(pathname)
   return (
     <div className="fixed bottom-0 left-0 right-0 flex justify-center sm:bottom-6">
-      <nav className="flex w-full justify-between bg-stone-50/55 dark:bg-stone-700/55 px-4 py-3 backdrop-blur sm:max-w-xs sm:rounded-full sm:shadow-lg sm:ring-2 sm:ring-stone-700/50">
-        {[
-          {
-            title: 'Pets',
-            href: '/admin/pets',
-            icon: PawPrint,
-          },
-          {
-            title: 'Colors',
-            href: '/admin/colors',
-            icon: SwatchBook,
-          },
-          {
-            title: 'Add new pet',
-            href: '/admin/newPet',
-            icon: Plus,
-          },
-          {
-            title: 'Search for pets or colors',
-            href: '/admin/search',
-            icon: Search,
-          },
-          {
-            title: 'Menu',
-            href: '/admin/menu',
-            icon: Menu,
-          },
-        ].map((item, index) => (
-          <Link key={index} href={item.href} title={item.title} className="group">
-            <item.icon
-              size={30}
-              absoluteStrokeWidth
-              className={clsx(
-                'relative drop-shadow transition-all group-hover:text-orange-600 sm:group-hover:-translate-y-0.5',
-                pathname == item.href ? 'text-orange-600' : 'text-stone-700'
-              )}
-            />
-          </Link>
-        ))}
+      <nav className="flex w-full justify-between bg-stone-50/55 px-4 py-3 backdrop-blur dark:bg-stone-700/55 sm:max-w-xs sm:rounded-lg sm:shadow-lg sm:ring-2 sm:ring-stone-700/50">
+        <>
+          {[
+            {
+              title: 'Pets',
+              href: '/admin/pets',
+              icon: PawPrint,
+            },
+            {
+              title: 'Colors',
+              href: '/admin/colors',
+              icon: SwatchBook,
+            },
+            {
+              title: 'Add new pet',
+              href: '/admin/newPet',
+              icon: Plus,
+            },
+            {
+              title: 'Search for pets or colors',
+              href: '/admin/search',
+              icon: Search,
+            },
+          ].map((item, index) => (
+            <Link key={index} href={item.href} title={item.title} className="group">
+              <item.icon
+                size={30}
+                absoluteStrokeWidth
+                className={clsx(
+                  'relative drop-shadow transition-all group-hover:text-orange-600 sm:group-hover:-translate-y-0.5',
+                  pathname == item.href ? 'text-orange-600' : 'text-stone-700'
+                )}
+              />
+            </Link>
+          ))}
+        </>
+        <>
+          <Menu>
+            <MenuButton className="group">
+              <MenuIcon
+                size={30}
+                absoluteStrokeWidth
+                className="drop-shadow transition-all group-hover:text-orange-600 group-data-[open]:text-orange-600 sm:group-hover:-translate-y-0.5"
+              />
+            </MenuButton>
+            <MenuItems
+              anchor={{ gap: 20, to: 'top', offset: -50 }}
+              className="flex min-w-36 flex-col gap-y-2.5 rounded-lg bg-stone-50/55 text-center shadow-lg ring-1 ring-orange-600 backdrop-blur dark:bg-stone-700/55"
+            >
+              <div className="flex">
+                <MenuItem as={Button} className="flex flex-1 justify-center p-2">
+                  <Sun size={30} absoluteStrokeWidth />
+                </MenuItem>
+                <MenuItem as={Button} className="flex flex-1 justify-center p-2">
+                  <SunMoon size={30} absoluteStrokeWidth />
+                </MenuItem>
+                <MenuItem as={Button} className="flex flex-1 justify-center p-2">
+                  <Moon size={30} absoluteStrokeWidth />
+                </MenuItem>
+              </div>
+              <div className="flex">
+                <MenuItem as={Link} href="/" className="flex flex-1 justify-center p-2">
+                  <Home size={30} absoluteStrokeWidth />
+                </MenuItem>
+                <MenuItem
+                  as={Link}
+                  href="/api/auth/signout"
+                  className="flex flex-1 justify-center p-2"
+                >
+                  <LogOut size={30} absoluteStrokeWidth />
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </Menu>
+        </>
       </nav>
     </div>
   )
