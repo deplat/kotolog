6299a84a51df29b29fd787a802c9a97dff652e5d
deplat <146228938+deplat@users.noreply.github.com>
2024-11-10 14:58:02 +0300

    Styling

--- app/(admin)/_modules/pet-card/PetCard.tsx
+++ app/(admin)/_modules/pet-card/PetCard.tsx
@@ -17,7 +17,7 @@ export const PetCard = ({ id, name, slug, avatarSrc }: PetCardProps) => {
         <div className="flex">
           <PetCardAvatar avatarSrc={avatarSrc} name={name} />
         </div>
-        <div className="flex w-full flex-col justify-between p-3">
+        <div className="flex w-full flex-col justify-evenly p-3">
           <div className="text-stone-600 dark:text-stone-400">#{id}</div>
           <div className="text-lg">{name}</div>
           <div className="text-stone-600 dark:text-stone-400">/{slug}</div>
