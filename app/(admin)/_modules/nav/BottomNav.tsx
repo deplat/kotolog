185618110cb272f88895d2b894bf34f9919314d9
deplat <146228938+deplat@users.noreply.github.com>
2024-11-11 18:24:16 +0300

    Fix

--- app/(admin)/_modules/nav/BottomNav.tsx
+++
@@ -1,8 +0,0 @@
-import { BottomNavControls } from '@/app/(admin)/_modules/nav/BottomNavControls'
-import { auth } from '@/auth'
-
-export const BottomNav = async () => {
-  const session = await auth()
-  if (session?.user.role == 'USER') return null
-  return <BottomNavControls />
-}
