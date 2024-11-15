4e60f3b276522b435f72ec1698cbd6a28921ab77
deplat <146228938+deplat@users.noreply.github.com>
2024-11-15 15:57:54 +0300

    Suspense fix

--- app/(pets)/sobaki/DogListLayout.tsx
+++ app/(pets)/sobaki/DogListLayout.tsx
@@ -1,6 +1,7 @@
 import { GiSittingDog } from 'react-icons/gi'
 import { DogFilters } from '@/app/(pets)/sobaki/DogFilters'
 import { FilterSidebar } from '@/app/(pets)/sobaki/FilterSidebar'
+import { Suspense } from 'react'
 
 export const DogListLayout = async ({
   availableDogGenders,
@@ -20,14 +21,16 @@ export const DogListLayout = async ({
       <div className="flex w-full items-center">
         <GiSittingDog size={36} className="me-2 text-gray-500 dark:text-gray-400 md:me-4" />
         <h1 className="text-4xl font-bold">Собаки</h1>
-        <FilterSidebar>
-          <DogFilters
-            availableDogGenders={availableDogGenders}
-            availableDogFurTypes={availableDogFurTypes}
-            availableDogAgeGroups={availableDogAgeGroups}
-            availableDogColors={availableDogColors}
-          />
-        </FilterSidebar>
+        <Suspense>
+          <FilterSidebar>
+            <DogFilters
+              availableDogGenders={availableDogGenders}
+              availableDogFurTypes={availableDogFurTypes}
+              availableDogAgeGroups={availableDogAgeGroups}
+              availableDogColors={availableDogColors}
+            />
+          </FilterSidebar>
+        </Suspense>
       </div>
       {children}
     </div>
